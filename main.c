a5f30c54302e9a1cd202a9ea1151a6744409bef4
dui-luis-alberto-37 <lgarciaorozco6@gmail.com>
2023-11-13 13:32:34 -0600

    C programs

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main(){
+    unsigned long int N;
+    unsigned long int i;
+    unsigned long int r;
+    
+    /*
+    int *n;
+    int *m;
+
+    m = malloc(9);
+    n = malloc(8);
+    *n = 3;
+    *m = 2;
+    printf("%i\n",*n+*m);
+    */
+
+    /*
+    N = 1000000000;
+    r = 0;
+
+    for(i = 1; i <= N; i ++){
+        r += i;
+    }
+
+    printf("Varlor int máximo: %lu\nr = %lu\n",LONG_MAX,r);
+    */
+
+    //printf("Hello World\n");
+    return r;
+}
